b46f9000d0c827bb9a6887bf0b8400461bac1297
sipper Super69 <sipperroblox@gmail.com>
2021-03-11 00:29:44 +0000

    YES

--- main.lua
+++ main.lua
@@ -1,2 +1,11 @@
 function love.load ()
+  
+end 
+
+function love.draw()
+  love.graphics.rectangle("fill",300,400,50,50)
+  love.graphics.circle("fill",300,500,60,60)
+  love.graphics.circle("fill",600,400,40,40)
+  love.graphics.print("HEY I SEE YOUR IP ADRESS LMAO EZ YOUR SUCH A FOOL THANKS FOR YOUR IP IM GONNA SELL THIS SHIT AND GAIN SOME CASH ON THE DARK WEB AHAHAHAH")
+  
   end 
